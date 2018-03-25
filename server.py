e974abca503acbc896b614066c746c010780eb9d
Mark Crossen <markcrossen@studentbody.byu.edu>
2018-03-24 18:52:34 -0700

    basic python server

---
+++ server.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    with open('index.html', 'r') as file:
+        return file.read()
+
+
+
+if __name__ == '__main__':
+    app.run()
