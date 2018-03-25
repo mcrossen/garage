b724a1eb9f1c35a33e36937db879ed0fc4a3d2e6
Mark Crossen <markcrossen@studentbody.byu.edu>
2018-03-24 19:04:52 -0700

    basic door open methods

--- server.py
+++ server.py
@@ -6,10 +6,25 @@ app = Flask(__name__)
 
 
 @app.route("/")
+@app.route("/index")
 def index():
     with open('index.html', 'r') as file:
         return file.read()
 
+
+@app.route("/gate", methods = ['POST'])
+def opengate():
+    return "opening gate..."
+
+
+@app.route("/leftgarage", methods = ['POST'])
+def openLeftGarage():
+    return "opening left garage..."
+
+
+@app.route("/rightgarage", methods = ['POST'])
+def openRightGarage():
+    return "opening right garage..."
 
 
 if __name__ == '__main__':
