59debbb582a48a3c6ec65db502a8c87fbc25ca45
Scott Crossen <scottcrossen42@gmail.com>
2023-03-12 18:38:45 -0700

    Made all doors generic instead of having 3 hard-coded doors

--- server.py
+++ server.py
@@ -5,104 +5,141 @@ from threading import Thread, Lock
 import RPi.GPIO as GPIO
 import click
 import smbus
-
+import json
+import logging
 
 DEFAULT_HOST = '0.0.0.0'
 DEFAULT_PORT = 8080
 DEFAULT_LANDING_PAGE = 'index.html'
 RELAY_ASSERT_DURATION = 0.5
+RELAY_POST_SLEEP_DURATION = 8
 RELAY_DEVICE_ADDR = 0x10
-# the I2C data addresses for each door
-GATE = 1
-LEFT_GARAGE = 2
-RIGHT_GARAGE = 3
-GATE_REED_SWITCH = 15
-# program variables
-app = Flask(__name__)
-relay_locks = {
-    GATE: Lock(),
-    LEFT_GARAGE: Lock(),
-    RIGHT_GARAGE: Lock(),
+doors = {
+  'garage': {
+    'relay_num': 1,
+    'sensor_pin': 15,
+    'lock': Lock()
+  }
 }
+app = Flask(__name__)
 bus = smbus.SMBus(1)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(GATE_REED_SWITCH, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+app.logger.setLevel(logging.INFO)
 
+def check_door_sensor_state(door_name):
+    gpio = doors[door_name]['sensor_pin']
+    state = GPIO.input(gpio)
+    if state == 1:
+        return 'open'
+    else:
+        return 'closed'
+
+for door_name in doors.keys():
+    if 'sensor_pin' in doors[door_name]:
+        GPIO.setup(doors[door_name]['sensor_pin'], GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+def check_door_state(door_name):
+    if 'transitioning' in doors[door_name]:
+        return 'indeterminate'
+    if 'sensor_pin' not in doors[door_name]:
+        return 'unknown'
+    if 'desired' in doors[door_name]:
+        return 'indeterminate'
+    return check_door_sensor_state(door_name)
+
+def toggle_door_state_unsafe(door_name):
+    relay = doors[door_name]['relay_num']
+    bus.write_byte_data(RELAY_DEVICE_ADDR, relay, 0xFF)
+    sleep(RELAY_ASSERT_DURATION)
+    bus.write_byte_data(RELAY_DEVICE_ADDR, relay, 0x00)
+    sleep(RELAY_POST_SLEEP_DURATION)
+
+def toggle_door_state_blocking(door_name):
+    with doors[door_name]['lock']:
+        toggle_door_state_unsafe(door_name)
+        del doors[door_name]['transitioning']
+    app.logger.info('finished state toggle for {}'.format(door_name))
+
+def change_door_state_unsafe(door_name, desired):
+    current = check_door_sensor_state(door_name)
+    if current == desired:
+        app.logger.info('extraneous state transition for {} (current: {}, desired: {})'.format(door_name, current, desired))
+        return
+    toggle_door_state_unsafe(door_name)
+    current = check_door_sensor_state(door_name)
+    last = doors[door_name]['last']
+    desired = doors[door_name]['desired']
+    if last == desired and current != desired:
+        doors[door_name]['last'] = current
+        app.logger.info('repeating state transition for {} (current: {}, desired: {})'.format(door_name, current, desired))
+        change_door_state_unsafe(door_name, desired)
+    elif last != desired and current == desired:
+        doors[door_name]['last'] = current
+        app.logger.info('successful state transition for {} (current: {})'.format(door_name, current))
+    else:
+        app.logger.error('state transition error for {} (current: {}, last: {}, desired: {})'.format(door_name, current, last, desired))
+
+def change_door_state_blocking(door_name, desired):
+    with doors[door_name]['lock']:
+        change_door_state_unsafe(door_name, desired)
+        del doors[door_name]['last']
+        del doors[door_name]['desired']
+    app.logger.info('finished state transition for {} (desired: {})'.format(door_name, desired))
+
+def change_door_state(door_name, desired):
+    if 'sensor_pin' not in doors[door_name] or desired == 'unknown':
+        app.logger.info('beginning state toggle for {}'.format(door_name))
+        doors[door_name]['transitioning'] = True
+        Thread(target=toggle_door_state_blocking, kwargs={'door_name':door_name}).start()
+    elif 'desired' in doors[door_name]:
+        doors[door_name]['desired'] = desired
+        current = check_door_sensor_state(door_name)
+        app.logger.info('queuing state transition for {} (current: {}, desired: {})'.format(door_name, current, desired))
+    else:
+        doors[door_name]['desired'] = desired
+        current = check_door_sensor_state(door_name)
+        doors[door_name]['last'] = current
+        app.logger.info('beginning state transition for {} (current: {}, desired: {})'.format(door_name, current, desired))
+        Thread(target=change_door_state_blocking, kwargs={'door_name':door_name, 'desired':desired}).start()
 
 @click.command()
 @click.option('--host', default=DEFAULT_HOST, help='What host to broadcast')
 @click.option('--port', default=DEFAULT_PORT, help='What port to bind to')
 @click.option('--landing-page', 'page',
     default=DEFAULT_LANDING_PAGE,
-    help="the webpage to serve on the main page")
-@click.option('--door-password', 'password', default='',
-    help="require users to open doors with a password")
-def run_server(host, port, page, password):
-    """easy web interface to control gate and garage doors"""
+    help='the webpage to serve on the main page')
+def run_server(host, port, page):
+    '''easy web interface to control gate and garage doors'''
     app.config['index'] = page
-    app.config['password'] = password
     app.run(host=host, port=port)
 
-
-def open_door(relay: int):
-    def assert_relay(relay: int):
-        with relay_locks.get(relay):
-            bus.write_byte_data(RELAY_DEVICE_ADDR, relay, 0xFF)
-            sleep(RELAY_ASSERT_DURATION)
-            bus.write_byte_data(RELAY_DEVICE_ADDR, relay, 0x00)
-    Thread(target=assert_relay, kwargs={'relay':relay}).start()
-
-def check_state(gpio: int):
-    state = GPIO.input(gpio)
-    if state == 1:
-        return "open"
-    else:
-        return "closed"
-
-@app.route("/")
-@app.route("/index")
+@app.route('/')
+@app.route('/index')
 def index():
     with open(app.config.get('index'), 'r') as file:
         return file.read()
 
-@app.route("/state", methods = ['GET'])
-def gate_state():
-    if app.config.get('password') != request.data.decode():
-        print("invalid password: {}".format(request.data))
-        return "unauthorized", 401
-    return "{{\"gate\": \"{}\"}}".format(check_state(GATE_REED_SWITCH))
+@app.route('/doors/<door_name>', methods = ['GET'])
+def door_state(door_name=None):
+    if door_name is None:
+        return 'bad request', 400
+    return '{{\"state\": \"{}\"}}'.format(check_door_state(door_name))
 
-@app.route("/open_gate", methods = ['POST'])
-def open_gate():
-    if app.config.get('password') != request.data.decode():
-        print("invalid password: {}".format(request.data))
-        return "unauthorized", 401
-    open_door(GATE)
-    return "{}"
+@app.route('/doors/<door_name>', methods = ['POST'])
+def open_door(door_name=None):
+    if door_name is None:
+        return 'bad request', 400
+    data = json.loads(request.data)
+    change_door_state(door_name, data['state'])
+    return '{{\"state\": \"{}\"}}'.format(check_door_state(door_name))
 
-@app.route("/open_left_garage", methods = ['POST'])
-def open_left_garage():
-    if app.config.get('password') != request.data.decode():
-        print("invalid password: {}".format(request.data))
-        return "unauthorized", 401
-    open_door(LEFT_GARAGE)
-    return "{}"
+@app.route('/doors', methods = ['GET'])
+def list_doors():
+    return json.dumps(list(doors.keys()))
 
-
-@app.route("/open_right_garage", methods = ['POST'])
-def open_right_garage():
-    if app.config.get('password') != request.data.decode():
-        print("invalid password: {}".format(request.data))
-        return "unauthorized", 401
-    open_door(RIGHT_GARAGE)
-    return "{}"
-
-
-@app.route("/health")
+@app.route('/health')
 def health():
-    return "1"
-
+    return '1'
 
 if __name__ == '__main__':
     run_server()
-
